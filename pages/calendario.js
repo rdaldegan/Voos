92856c424f0d36d6230e1ae4ee33eb1ce8fcb4bc
Rafael Barreto Daldegan <ra.fa.daldegan@hotmail.com>
2021-07-22 11:46:25 -0300

    .

--- pages/calendario.js
+++ pages/calendario.js
@@ -30,6 +30,7 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+
     .warning{
       font-size: 2rem;
       width: 80%;
@@ -88,7 +89,7 @@ export async function getStaticProps() {
     props: {
       nextEvents,
     },
-    revalidate: 3600,
+    revalidate: 1,
   };
 }
 
@@ -116,17 +117,24 @@ export default function Calendario({ nextEvents }) {
       <div className="header">
         <h2>Próximos eventos:</h2>
       </div>
-      {nextEvents.length > 0 && nextEvents.map((event) => (
+      {nextEvents.length > 0 && nextEvents.map(({
+        eventPageHref,
+        logoImg,
+        eventName,
+        coverImg,
+        eventTheme,
+        eventDate,
+      }) => (
         <EventCard
-          key={event.name}
-          img={event.img}
-          name={event.name}
-          href={event.href}
-          backgroundImg={event.backgroundImg}
-          theme={event.theme}
-          dia={event.dia}
-          mes={event.mes}
-          ano={event.ano}
+          key={eventPageHref}
+          img={logoImg}
+          name={eventName}
+          href={`/nextEvents/${eventPageHref}`}
+          backgroundImg={coverImg}
+          theme={eventTheme}
+          dia={eventDate.dia}
+          mes={eventDate.mes}
+          ano={eventDate.ano}
         />
       ))}
 
